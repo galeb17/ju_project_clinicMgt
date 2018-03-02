ffee6212522155ced356887873f99ea5a48f9cf0
abdullah.galeb <galeb.online@gmail.com>
2018-03-02 21:52:37 +0600

    Initial application generated by JHipster-4.10.2

---
+++ src/main/java/com/pgdit/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.pgdit.repository;
+
+import com.pgdit.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('clinicApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
